76887c21f4dbdec2f3a569bbc3e0d1c3d4978cde
anmolchhabra21 <anmolchhabra71500@gmail.com>
2024-02-04 23:36:25 +0530

    added selectedDate filter on DB, reducing queries on DB

--- src/backend/backend.controller.ts
+++ src/backend/backend.controller.ts
@@ -2,65 +2,84 @@ import { Controller, Post, Body, Get, Param, Query } from '@nestjs/common';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
 import { BackendService } from './backend.service.js';
+import * as moment from 'moment';
 
 @Controller('backend')
 export class BackendController {
-    constructor(private configService: ConfigService, private backendservice: BackendService) {}
-// @Post('query')
-// async query(@Body() requestBody: any): Promise<any> {
-//     try{
-//         const openaiResponse = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
-//             prompt: requestBody.prompt,
-//         });
+  constructor(
+    private configService: ConfigService,
+    private backendservice: BackendService,
+  ) {}
+  // @Post('query')
+  // async query(@Body() requestBody: any): Promise<any> {
+  //     try{
+  //         const openaiResponse = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+  //             prompt: requestBody.prompt,
+  //         });
 
-//         const requestData = {
-//             prompt: requestBody.prompt,
-//             tokensCount: openaiResponse.data.choices[0].usage.total_tokens,
-//             responseLatency: openaiResponse.headers['x-response-time'],
-//             metadata: requestBody.metadata,
-//             success: true,
-//         };
-//         logToClickhouse(requestData);
+  //         const requestData = {
+  //             prompt: requestBody.prompt,
+  //             tokensCount: openaiResponse.data.choices[0].usage.total_tokens,
+  //             responseLatency: openaiResponse.headers['x-response-time'],
+  //             metadata: requestBody.metadata,
+  //             success: true,
+  //         };
+  //         logToClickhouse(requestData);
 
-//         return openaiResponse.data;
-//     } catch (error) {
-//         // Handle errors
-//         const requestData = {
-//           prompt: requestBody.prompt,
-//           tokensCount: 0,
-//           responseLatency: 0,
-//           metadata: requestBody.metadata,
-//           success: false,
-//         };
-//         logToClickhouse(requestData);
-    
-//         throw new Error('Internal Server Error');
-//       }
-//     }
-// }
-    @Get()
-    test() {
-        const test1 = this.configService.get<string>('CLICKHOUSE_USERNAME');
-        return "Hello from here " + test1;
-    }
-    @Post('db')
-    async insertDb(@Body() body) {
-        const book = await this.backendservice.insertdB(body.query);
-        return book;
-    }
-    // @Get('Testdb')
-    // async testDb() {
-    //     const book = await this.backendservice.testGPT();
-    //     return book;
-    // }
-    @Get('gpt')
-    async getHello(@Query('query') query: string) {
-        if (query) {
-            const book = await this.backendservice.testGPT(query);
-            return book;
-        } else {
-        return 'Query Shouldnt be empty!';
-        }
-    }
+  //         return openaiResponse.data;
+  //     } catch (error) {
+  //         // Handle errors
+  //         const requestData = {
+  //           prompt: requestBody.prompt,
+  //           tokensCount: 0,
+  //           responseLatency: 0,
+  //           metadata: requestBody.metadata,
+  //           success: false,
+  //         };
+  //         logToClickhouse(requestData);
 
+  //         throw new Error('Internal Server Error');
+  //       }
+  //     }
+  // }
+  @Get()
+  test() {
+    const test1 = this.configService.get<string>('CLICKHOUSE_USERNAME');
+    return 'Hello from here ' + test1;
+  }
+  @Post('db')
+  async insertDb(@Body() body) {
+    const book = await this.backendservice.insertdB(body.query);
+    return book;
+  }
+  // @Get('Testdb')
+  // async testDb() {
+  //     const book = await this.backendservice.testGPT();
+  //     return book;
+  // }
+  @Post('gpt')
+  async getByDate(@Body() body) {
+    console.log("anml", body);
+    if(!("startDate" in body) || !("endDate" in body)){
+      body["startDate"] = moment().subtract(1, 'days');;
+      body["endDate"] = moment();
+    }
+    body["startDate"] = moment(body["startDate"]).format('YYYY-MM-DD HH:MM:SS');
+    // body["startDate"] = moment(body["startDate"]);
+    // console.log("piyush", body["startDate"])
+    body["endDate"] = moment(body["endDate"]).format('YYYY-MM-DD HH:MM:SS');
+    console.log("piyush", body["startDate"], body["endDate"])
+
+    const book = await this.backendservice.getByDate(body["startDate"], body["endDate"]);
+    return book;
+  }
+  @Get('gpt')
+  async getHello(@Query('query') query: string) {
+    if (query) {
+      const book = await this.backendservice.testGPT(query);
+      return book;
+    } else {
+      return 'Query Shouldnt be empty!';
+    }
+  }
 }
--- src/backend/backend.service.ts
+++ src/backend/backend.service.ts
@@ -1,52 +1,81 @@
 import { Injectable } from '@nestjs/common';
 import { createClient } from '@clickhouse/client';
 import { ConfigService } from '@nestjs/config';
-import OpenAI from "openai";
+import OpenAI from 'openai';
+import moment from 'moment';
 
 @Injectable()
 export class BackendService {
-    private client;
-    private openai;
+  private client;
+  private openai;
 
-    constructor(private configService: ConfigService) {
-        this.openai = new OpenAI({apiKey: this.configService.get<string>('GPT_KEY')});
-        this.client = createClient({
-            host: this.configService.get<string>('CLICKHOUSE_HOST') ?? 'http://localhost:8123',
-            username: this.configService.get<string>('CLICKHOUSE_USERNAME') ?? 'default',
-            password: this.configService.get<string>('CLICKHOUSE_PASSWORD') ?? '',
-        })
-    }
+  constructor(private configService: ConfigService) {
+    this.openai = new OpenAI({
+      apiKey: this.configService.get<string>('GPT_KEY'),
+    });
+    this.client = createClient({
+      host:
+        this.configService.get<string>('CLICKHOUSE_HOST') ??
+        'http://localhost:8123',
+      username:
+        this.configService.get<string>('CLICKHOUSE_USERNAME') ?? 'default',
+      password: this.configService.get<string>('CLICKHOUSE_PASSWORD') ?? '',
+    });
+  }
 
-    async insertdB(request: string) {
-        const data = await this.testGPT(request);   
-        const test = await this.client.insert({
-            table: 'gpt_response_db',
-            // structure should match the desired format, JSONEachRow in this example
-            values: [
-                {   
-                    request: request,
-                    model: data.model,
-                    status: true,
-                    prompt_tokens: data.usage.prompt_tokens,
-                    completion_tokens: data.usage.completion_tokens,
-                    total_tokens: data.usage.total_tokens,
-                    response: data.choices[0].message.content,
-                    created_at: "2019-01-01 00:00:00"
-                }
-            ],
-            format: 'JSONEachRow',
-        })
-        console.log("1st entry", test);
-        return test;
-    }
+  async insertdB(request: string) {
+    const data = await this.testGPT(request);
+    const test = await this.client.insert({
+      table: 'gpt_response_db',
+      // structure should match the desired format, JSONEachRow in this example
+      values: [
+        {
+          request: request,
+          model: data.model,
+          status: true,
+          prompt_tokens: data.usage.prompt_tokens,
+          completion_tokens: data.usage.completion_tokens,
+          total_tokens: data.usage.total_tokens,
+          response: data.choices[0].message.content,
+          created_at: moment(data.created).format('YYYY-MM-DD HH:MM:SS'),
+        },
+      ],
+      format: 'JSONEachRow',
+    });
+    console.log('1st entry', test);
+    return test;
+  }
 
-    async testGPT(query: string){
-        const completion = await this.openai.chat.completions.create({
-            messages: [{ role: "system", content: query }],
-            model: "gpt-3.5-turbo",
-        });
-    
-        console.log(completion);
-        return completion;
+  async testGPT(query: string) {
+    const completion = await this.openai.chat.completions.create({
+      messages: [{ role: 'system', content: query }],
+      model: 'gpt-3.5-turbo',
+    });
+
+    console.log(completion);
+    return completion;
+  }
+
+  async getByDate(startDate: string, endDate: string) {
+    try {
+      console.log(startDate, endDate);
+      const query = `
+          SELECT *
+          FROM gpt_response_db
+          WHERE created_at >= toDateTime('${startDate}') AND created_at <= toDateTime('${endDate}');
+          `;
+
+      const result = await this.client.query({
+        query: query,
+        format: 'JSONEachRow',
+      });
+      const data = await result.json();
+      return data;
+
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+      throw error;
     }
+  }
 }
